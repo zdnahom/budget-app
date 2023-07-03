b508fb82b7efdef23e211f388c8dd4476634b020
Nahom <zerihunnahom@gmail.com>
2023-07-03 17:29:52 +0300

    Add relationship with expense_category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :author_id
+    has_many :expense_categories
+    has_many :expenses, through: :expense_categories
 
     validates :name, :icon, presence: true
     # validates :icon, format: { with: /\.(png|jpg|jpeg|gif)\z/i, message: 'must be a valid image URL' }
