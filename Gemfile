d288388d71e5c9542af195134879a24cd79c91ae
Nahom <zerihunnahom@gmail.com>
2023-07-04 08:53:40 +0300

    Fix linters

--- Gemfile
+++ Gemfile
@@ -72,6 +72,6 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+gem 'cancancan'
 gem 'devise'
 gem 'rubocop', '>= 1.0', '< 2.0'
-gem 'cancancan'
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,11 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-      return unless user.present?
-      can :manage, :all
+    return unless user.present?
+
+    can :manage, :all
   end
 end
