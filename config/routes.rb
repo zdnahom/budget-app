f84a39f36f8141f4727db8b372fd5a36f276a239
Nahom <zerihunnahom@gmail.com>
2023-07-04 09:38:18 +0300

    Add routes to categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/new'
+  get 'categories/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "categories#index"
+  resources :categories, only: [:index, :new, :create]
 end
