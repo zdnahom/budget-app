1426665565673952b6f947e7bf77a0f2ad6402ae
Nahom <zerihunnahom@gmail.com>
2023-07-04 12:43:59 +0300

    Add routes to expenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'expenses/index'
+  get 'expenses/new'
+  get 'expenses/create'
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "categories#index"
-  resources :categories, only: [:index, :new, :create]
+  resources :categories, only: [:index, :new, :create] do
+    resources :expenses, only: [:index, :new, :create]
+  end
 end
