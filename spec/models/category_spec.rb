2ce2a6bfe4e6e8af0e33d7df0b3c74ccfc29f32b
Nahom <zerihunnahom@gmail.com>
2023-07-04 08:01:35 +0300

    Update specs based on devise

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  include Devise::Test::IntegrationHelpers
   # tests go here
   before :each do
-    @user = User.create(name: 'Tom')
+    @user = User.create(name: 'Tom', email: 'nahom@gmail.com', password: '123456',confirmed_at: Time.now)
+    sign_in :@user
     @category = Category.create(author: @user, name: 'category1', icon: 'https://i.ibb.co/gm68B4C/Mc-Donalds.png')
   end
 
--- spec/models/expense_category_spec.rb
+++ spec/models/expense_category_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ExpenseCategory, type: :model do
+  include Devise::Test::IntegrationHelpers
   # tests go here
   before :each do
-    @user = User.create(name: 'Tom')
+    @user = User.create(name: 'Tom', email: 'nahom@gmail.com', password: '123456',confirmed_at: Time.now)
+    sign_in :@user
     @category = @user.categories.new(name: 'category1', icon: 'https://i.ibb.co/gm68B4C/Mc-Donalds.png')
     @expense = @user.expenses.new(name: 'expense1', amount: 4.0)
     @expense_category = ExpenseCategory.new(expense: @expense, category: @category)
